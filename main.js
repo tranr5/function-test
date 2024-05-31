f57ba6a21f6a72c694f22989d5db221dda17eaa3
tranr5 <tranr4@gmail.com>
2024-05-31 11:19:26 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// console.log("hi")
+
+function computeArea (width,height) {
+
+    let whArea = width * height
+            
+    console.log("area of "+ width + " and "+ height + " is "  + whArea)
+}
+
+console.log(computeArea(5,5))
